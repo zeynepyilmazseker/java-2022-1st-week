7d09b0fda27314b0513038834f4b167e2a9729ac
zeynepyilmazseker <113781475+zeynepyilmazseker@users.noreply.github.com>
2022-09-24 12:19:06 +0300

    Add files via upload
    java 2022 1.hafta

---
+++ conditinionals/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi =11;
+        if(sayi<20) {
+            System.out.println("sayı 20den küçüktür");
+        } else if (sayi==20) {
+            System.out.println("sayı 20ye eşittit.");
+
+        }
+           else {
+               System.out.println("sayi 20den büyüktür.");
+        }
+        }
+    }
---
+++ datatypes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi=12 ;
+        char karakter ='A';
+        String sehir ="ankara";
+        boolean dogruMu= false ;
+        String metin ="hoşgeldin";
+        System.out.println("hoşgeldin");
+
+
+
+    }
+}
